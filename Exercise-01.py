1b29a1700b5c765a2b735a9f8f3bc05c0b8ac196
fahrulkharis <fahruljuwana@gmail.com>
2023-03-15 22:08:05 +0700

    menambah file

---
+++ Exercise-01.py
@@ -0,0 +1,19 @@
+print ("=================================")
+print ("Nama            = Fahrul Kharis Afandi")
+print ("NIM             = I0322041")
+print ("Tempat tinggal  = Juwana, Pati, Jawa Tengah")
+
+#Deskripsi dan visualisasi tempat tinggal
+print ("Pati adalah kota kelahiran saya. Saya dibesarkan di kota ini. Kota ini terletak di bagian utara Pulau Jawa yang dilalui oleh Jalan Pantura.")
+print ("Pati Bumi Mina Tani adalah julukan yang melekat dengan Kota Pati. Pada dasarnya, slogan ini sesuai dengan mata pencaharian mayoritas penduduk Pati yaitu petani.")
+print ("Kecamatan Juwana merupakan salah satu kecamatan di Kabuputen Pati. Pada awalnya kecamatan ini adalah sebuah kota administratif di abad pertengahan dan masa kolonial.")
+print ("Sisa-sisa kejayaan kota masa lalu dapat kita jumpai di kota ini. Stasiun Juwana masih berdiri kokoh namun tidak berfungsi seperti dulu. Stasiun ini merupakan stasiun penting SJS, Samarang Joana Stoomtram Maatschappij.")
+print ("Bekas kota lama beserta bangunan kolonial masih berdiri kokoh nan megah.")
+print ("Pecinan atau Chinatown berada di pusat kota dan jalan arteri. Banyak pertokoan dan arsitektur China beserta klenteng di sepanjang jalan.")
+print ("Proliman merupakan pusat jajanan atau street food yang ramai disaat malam hari. Kawasan ini terdapat taman yaitu Taman Krisna yang terdapat patung Krisna di tengahnya.")
+print ("Terdapat pusat perbelanjaan yang kompleks yaitu Pasar Juwana Baru. Pasar ini merupakan pasar induk di Kabupaten Pati dengan komoditas utama yaitu ikan.")
+print ("Sungi Silugonggo merupakan sungai raya di Jawa bagian utara. Sungai ini bermuara di pesisir pantai Juwana. Bahkan nama Silugonggo pun diabadikan menjadi nama jalan di kota ini.")
+
+#Harapan setelah lulus dari teknik industri
+print ("Setelah lulus dari teknik industri saya berharap dapat memiliki pengalaman di perusahaan multinasional. Unilever, Pertamnina, Indofood, dan Schlumberger merupakan perusahaan impian saya.")
+print ("Pengalaman tersebut akan berguna bagi saya untuk apply untuk study lanjut S2 Business School.")
